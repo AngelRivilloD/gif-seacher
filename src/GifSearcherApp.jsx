0f29101a03e3aac5bdc26872fb0b72c4a652417e
Angel Antonio Rivillo Diaz <antorivillo@gmail.com>
2023-03-06 16:52:03 +0100

    Primer commit

---
+++ src/GifSearcherApp.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { AddCategory, GifGrid } from './components';
+
+export const GifSearcherApp = () => {
+
+    const [categories, setCategories] = useState(['Dragon Ball']);
+
+    const onAddCategory = (newCategory) => {
+
+        if (categories.includes(newCategory)) return;
+
+        setCategories([...categories, newCategory]);
+    }
+
+    return (
+        <>
+            <h1>Gif seacher</h1>
+
+            <AddCategory
+                onNewCategory={onAddCategory}
+            />
+
+            {
+                categories.map(category => (
+                    <GifGrid key={category} category={category} />
+                ))
+            }
+        </>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GifSearcherApp } from './GifSearcherApp'
+
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GifSearcherApp />
+  </React.StrictMode>
+)
